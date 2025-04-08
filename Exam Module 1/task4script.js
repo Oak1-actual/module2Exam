fe71628789cfdcac655fe195be15db7467dc754c
Oak1-actual <tord.eiken@hotmail.com>
2025-04-08 15:53:26 +0200

    Change

--- Exam Module 1/task4script.js
+++ Exam Module 1/task4script.js
@@ -1,13 +1,27 @@
+// Global data
 let listings = [];
 
+// Load page
+window.onload = function () {
+  listingsFromAPI();
+};
+
+// fetch listing data
 function listingsFromAPI() {
   fetch("https://api.npoint.io/eb3c116538e7dcbfc7bf")
     .then(response => response.json())
     .then(data => {
       listings = data; // Stores data in global variable.
+      renderListingCards();
     });
 }
 
-window.onload = function () {
-    listingsFromAPI();
-  };
+// Display listing cards w/API data
+function renderListingCards() {
+  const flexbox = document.getElementById("flexbox-w-cards");
+  // create card
+  let listingCard = document.createElement("div");
+  listingCard.id = "listing-card";
+  // Add content to card
+  console.log("this is listings:", listings);
+}
