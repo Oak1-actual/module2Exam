b1ab2daf139323e3af7f5622d61f24119f131983
Oak1-actual <tord.eiken@hotmail.com>
2025-04-09 09:26:15 +0200

    Update task4script.js

--- Exam Module 1/task4script.js
+++ Exam Module 1/task4script.js
@@ -18,10 +18,25 @@ function listingsFromAPI() {
 
 // Display listing cards w/API data
 function renderListingCards() {
-  const flexbox = document.getElementById("flexbox-w-cards");
-  // create card
-  let listingCard = document.createElement("div");
-  listingCard.id = "listing-card";
-  // Add content to card
-  console.log("this is listings:", listings);
+  listings.forEach(listing => {
+    const flexbox = document.getElementById("flexbox-w-cards");
+    // create card
+    let listingCard = document.createElement("div");
+    listingCard.id = "listing-card";
+    // Add Title to card
+    let listingCardTitle = document.createElement("h3");
+    listingCardTitle.textContent = listing.title;
+    // Add Image to card
+    let listingCardImage = document.createElement("img");
+    listingCardImage.src = listing.media.images[0];
+    // Add Text to card
+    let listingCardText = document.createElement("p");
+    listingCardText.textContent = `housing type: ${listing.type}`;
+    // Append to card
+    listingCard.appendChild(listingCardImage);
+    listingCard.appendChild(listingCardTitle);
+    listingCard.appendChild(listingCardText);
+    // Append to Flexbox
+    flexbox.appendChild(listingCard);
+  })
 }
